e351b8935ca9fe727442471bfac97a9e194bc13a
kak79 <kohelka@gmail.com>
2020-06-18 14:34:02 -0500

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,27 @@
+require "pry"
+def reverse_each_word(sentence)
+
+    words = sentence.split(" ") 
+    the_backward_sentencea = ""
+   
+    words.each do | word |
+#binding.pry
+        
+        the_backward_sentencea << word.reverse + " " 
+    
+    end
+
+    p the_backward_sentencea.chop
+
+    the_backward_sentenceb = ""
+    words.collect do | word |
+
+        the_backward_sentenceb << word.reverse + " " 
+
+    end
+
+    p the_backward_sentenceb.chop
+
+end
+
+
