0e16d4b8091ef6e93f8407adea8e3d155fcd1f13
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-06-17 19:55:30 -0700

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: false
 
-def reverse_each_word(sentence_string)
-  sentence_array = sentence_string.split(/ /)
-  sentence_array.collect(&:reverse).join(' ')
+def reverse_each_word(sentence)
+  sentence.split.collect(&:reverse).join(' ')
 end
